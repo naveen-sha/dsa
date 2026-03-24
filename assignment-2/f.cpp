c7175608fef6997c17096eecd0f70f5b820d6015
naveen-sha <naveen972821@gmail.com>
2026-03-24 20:49:11 +0530

    add question

---
+++ assignment-2/f.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+// Sum of First N Natural Numbers using formula
+
+int main(){
+    int N;
+    cin >> N;
+    int sum = (N*(N+1))/2;
+    cout << sum;
+    return 0;
+}
