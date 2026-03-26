ae1f6c4ba2c8e36e11e02745c9ebac6efe9a1c4c
naveen-sha <naveen972821@gmail.com>
2026-03-26 20:41:21 +0530

    comment out code

--- assignment-1/d.cpp
+++ assignment-1/d.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 int main(){
@@ -8,4 +8,4 @@ int main(){
    cout << " *" << endl;
    cout << "*****" << endl;
     return 0;
-   }
+   }*/
