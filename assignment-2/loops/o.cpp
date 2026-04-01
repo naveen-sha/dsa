a0ab416350883c4a98f662d0763737bdde795dc9
naveen-sha <naveen972821@gmail.com>
2026-04-01 08:57:54 +0530

    add question

---
+++ assignment-2/loops/o.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// print number in reverse order.
+
+/*int main(){
+    int n;
+    cout<<"enter a number:";
+    cin>>n;
+    for (int i=0;n!=0;i++){
+        cout<<n%10;
+        n=n/10;
+    }
+    return 0;
+
+}*/
