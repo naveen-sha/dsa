768dd0e356c9a6ec50649aca05745f89c593aa79
naveen-sha <naveen972821@gmail.com>
2026-04-01 08:05:29 +0530

    add question

--- assignment-2/loops/c.cpp
+++ assignment-2/loops/c.cpp
@@ -1,4 +1,15 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 // print all even numbers from 1 to N.
+int main(){
+    int n;
+    cout <<"enter the number:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        if(i%2==0){
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}*/
