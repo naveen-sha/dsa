5bc44cae4250f2685578f24a1da0d4f618d77439
naveen-sha <naveen972821@gmail.com>
2026-04-01 08:02:15 +0530

    add question

--- assignment-2/loops/b.cpp
+++ assignment-2/loops/b.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 // print N to 1;
-
+int main(){
+    int n;
+    cout<<"enter a number:";
+    cin>>n;
+    for(int i=n;i>=1;i--){
+        cout<<i<<" ";
+ 
+    }
+    return 0;
+}*/
 
 
