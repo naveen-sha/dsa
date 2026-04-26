2bfde4a40d7ad17a93336a56c25efeefd693bca9
naveen-sha <naveen972821@gmail.com>
2026-04-26 10:49:02 +0530

    fix the code

--- assignment-3/pattern-printing/d.cpp
+++ assignment-3/pattern-printing/d.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(i==0 || i==n-1 || j==0 || j==n-1)
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i==1 or i==n or j==1 or j==n)
                 cout << "*";
             else
                 cout << " ";
