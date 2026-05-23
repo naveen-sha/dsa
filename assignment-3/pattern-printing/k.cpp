8b7be5154ed80856e2920d633d9d474b2c2c12c2
naveen-sha <naveen972821@gmail.com>
2026-05-23 17:21:59 +0530

    add question

--- assignment-3/pattern-printing/k.cpp
+++ assignment-3/pattern-printing/k.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 using namespace std;
 
-// function to print the pattern
-int main() {
-    int n;
-    cout << "Enter the number of rows: ";
-    cin >> n;
-
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= i; j++) {
-            cout << "* ";
-        }
-        cout << endl;
+// hollow vertical triangle pattern
+ 
+int main(){
+   int n;
+   cin>>n; 
+   for(int i=n;i>=1;i--){
+    for(int j=1;j<=i;j++){
+        cout <<"*";
     }
-
-    return 0;
+    cout <<endl;
+   }
+   return 0;
 }
