300c1b153b238dc0386e7aae9b4fa0dc23b4c55c
naveen-sha <naveen972821@gmail.com>
2026-05-27 22:18:32 +0530

    add question

--- assignment-3/pattern-printing/k.cpp
+++ assignment-3/pattern-printing/k.cpp
@@ -6,7 +6,17 @@ using namespace std;
 int main(){ 
     int n;
     cin >>n;
-    
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=i;j++){
+            if(j==0 || j==i || i==n){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
 
     
    
