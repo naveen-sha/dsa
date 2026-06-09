ba107d7a0a7c192adcecac398a14e6158385b253
naveen-sha <naveen972821@gmail.com>
2026-06-09 11:38:20 +0530

    add question

--- assignment-3/pattern-printing/k.cpp
+++ assignment-3/pattern-printing/k.cpp
@@ -3,17 +3,35 @@ using namespace std;
 
 // hollow vertical triangle pattern
  
-int main(){ 
+int main(){
     int n;
-    cin>>n;
-    for(int i=0;i<=n;i++){
+    cin >> n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            if(j==0 || j==i){
+                cout << "* ";
+            }
+            else{
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+    for(int i=n-1;i>0;i--){
+        for(int j=0;j<i;j++){
+            if(j==0 || j==i-1){
+                cout << "* ";
+            }
+            else{
+                    cout << "  ";
+                }
+            }
+            cout << endl;
+        }
         
+        return 0;
     }
 
-
-
-   
-    }
 
     
    
