c8f5ee0302dc4b2bacee0045738ef0408bc2adf7
naveen-sha <naveen972821@gmail.com>
2026-06-10 10:05:21 +0530

    add question

---
+++ assignment-4/contest-1/a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// hello codeforces
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        cout << "Hello Codeforces " << i << endl;
+    }
+    return 0;
+}
