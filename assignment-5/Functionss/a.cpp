581cebd1543337a11ee696388d8c2c02933e9cba
naveen-sha <naveen972821@gmail.com>
2026-06-11 20:58:47 +0530

    add question

---
+++ assignment-5/Functionss/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// hello function
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout <<"I am learning functions"<<endl;
+
+    }
+    cout <<endl;
+    return 0;
+}
