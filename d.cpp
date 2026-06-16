1f46cb476416d0f5c7a957f8360a8027269f4b8d
naveen-sha <naveen972821@gmail.com>
2026-06-16 13:21:47 +0530

    add function topic with binomial coefficient question

--- d.cpp
+++ d.cpp
@@ -5,7 +5,28 @@ using namespace std;
 // -----------100xdsa------------------------.
 
 // binomial coefficient
+ int fact(int n){
+    int ans=1;
+    for(int i=1;i<=n;i++){
+        ans *= i;
 
+    }
+    return ans;
+ }
+
+ int main(){
+    int n,r;
+    cin>> n>>r;
+    // n!
+    int nfact=fact(n);
+    // r!
+    int rfact=fact(r);
+    // nrfact!
+    int nrfact=fact(n-r);
+    cout<< nfact/(rfact*nrfact)<<endl;
+    
+
+ }
 
 
 
