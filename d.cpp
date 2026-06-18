1c0225c7f6cc4c0da65ddce253634acff468ef69
naveen-sha <naveen972821@gmail.com>
2026-06-18 12:21:27 +0530

    add factor with count

--- d.cpp
+++ d.cpp
@@ -107,6 +107,18 @@ int main(){
   return 0;
 }*/
 
+int main(){
+  int n;
+  cin>> n;
+  int count=0;
+  for(int i=1;i<=n;i++){
+    if(n%i==0){
+      count++;
+    }
+  }
+  cout <<count<<endl;
+}
+
 
    
    
