effd6c93f6edcc2751e69dd558132710194f37d1
naveen-sha <naveen972821@gmail.com>
2026-07-02 12:23:17 +0530

    add question

--- assignment-5/Functionss/h.cpp
+++ assignment-5/Functionss/h.cpp
@@ -2,3 +2,28 @@
 using namespace std;
 
 // count zeros.
+
+# define int long long 
+
+int countzeros(int n){
+    int count=0;
+    if(n==0){
+        return 1;
+    }
+    for(int i=1;n!=0;i++){
+        if(n%10==0){
+            count++;
+        }
+        n=n/10;
+    }
+   
+    return count;
+}
+
+signed main(){
+    int n;
+    cin>> n;
+    int count=countzeros(n);
+    cout<< count <<endl;
+  
+}
