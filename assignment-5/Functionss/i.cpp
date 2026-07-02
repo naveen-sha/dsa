e8bd6f709c459de7d5071ecbe00bbd6d0e5d9607
naveen-sha <naveen972821@gmail.com>
2026-07-02 13:16:38 +0530

    add question

--- assignment-5/Functionss/i.cpp
+++ assignment-5/Functionss/i.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
 using namespace std;
+
+// find hcf
+
+#define int long long
+
+int findhcf(int a,int b){
+    for(int i=1; b!=0;i++){
+    int temp=a%b;
+    a=b;
+    b=temp;
+
+    }
+    return a;
+}
+
+signed main(){
+    int a,b;
+    cin>> a >> b;
+    int hcf=findhcf(a,b);
+    cout<< hcf <<endl;
+  
+}
